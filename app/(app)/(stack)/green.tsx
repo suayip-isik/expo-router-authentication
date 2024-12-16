6523f465f753a4dbc981d61755a96eea62a1598c
suayip-isik <suayip.isik.16@gmail.com>
2024-12-16 16:59:39 +0300

    code fix

--- app/(app)/(stack)/green.tsx
+++ app/(app)/(stack)/green.tsx
@@ -1,19 +1,15 @@
-import { Button, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import React from "react";
-import { Redirect, useRouter } from "expo-router";
+import { useRouter } from "expo-router";
 
 const GreenScreen = () => {
   const router = useRouter();
   return (
     <View>
       <Text>This is the green screen</Text>
-      <Text onPress={() => router.push("/(app)/(tabs)")}>
+      <Text onPress={() => router.dismissTo("/(app)/(tabs)")}>
         Go to blue screen
       </Text>
-      <Button
-        title="Go to blue screen"
-        onPress={() => <Redirect href="/(app)/(tabs)/explore" />}
-      />
     </View>
   );
 };
